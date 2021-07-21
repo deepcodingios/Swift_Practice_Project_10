37f948d4729845a2b2673095131d9ea76e3f08c7
Deepu <pradeep.kypa@voxvalley.com>
2021-07-21 15:12:42 +0530

    Initial Project Structure

---
+++ Swift_Practice_Project_10/Swift_Practice_Project_10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Practice_Project_10
+//
+//  Created by Pradeep Reddy Kypa on 21/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
